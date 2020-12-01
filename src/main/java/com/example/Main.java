5bb086dcda4aa81479afc2a9d5ef81e380c95852
Antti Rastas <antti.rastas@eficode.com>
2020-12-01 10:04:05 +0200

    Add gateway subscription to display events

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,19 +15,6 @@ public class Main {
   }
 
   public static void main(String[] args) throws Exception {
-    /*
-    // Listener services use:
-    Service.service(new Subscription[]{
-        new Subscription("user-history", "buy-product", (body, sender) -> {
-          System.out.println("user-history: buy-product");
-        }),
-        new Subscription("user-history", "login", (body, sender) -> {
-          System.out.println("user-history: login");
-          sender.send("buy-product", "Coffee");
-        })
-    });
-    */
-
     // Gateway service uses:
     Subscription.Sender globalSender = Service.service(new Subscription[]{
         new Subscription("display", "display", (body, sender) -> {
